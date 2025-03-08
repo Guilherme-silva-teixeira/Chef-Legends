608fab4c07075bdaf1010bdcf19bd47e3d39472d
Guilherme Teixeira <132455461+Guilherme-silva-teixeira@users.noreply.github.com>
2025-03-08 16:17:13 -0300

    Update Game.cs

--- Chef Legends/Game.cs
+++ Chef Legends/Game.cs
@@ -10,13 +10,25 @@ class Game
         initWindow init = new initWindow();
         Raylib.InitWindow(800, 600, "Game");
         Raylib.SetTargetFPS(60);
+        Pizza pizza = new Pizza(0,0,0,0.0f);
+        int starts = 0;
 
-        while(!Raylib.WindowShouldClose())
+        if ((init.StartCollision == false || init.optionCollsion == false) || starts < 1)
         {
-            init.Update();
             init.Draw();
         }
 
+        while (!Raylib.WindowShouldClose())
+        {
+            init.Update();
+            if (init.StartCollision == true)
+            {
+                starts += 1;
+                init.Delete();
+                pizza.Draw();
+            }
+
+        }
 
         Raylib.CloseWindow();
     }
